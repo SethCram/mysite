14b896737d10802cd3d9f0fc2f47b1a2d5afc715
Seth <sethcram@gmail.com>
2022-03-31 19:48:58 -0700

    added gitignore and secret key covered up

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3t&!_qn&_y&n&pdum2q7g9k*=^lu!@exwyz6tu*5kfqhd%8jwx'
+SECRET_KEY = config("SECRET_KEY") #'django-insecure-3t&!_qn&_y&n&pdum2q7g9k*=^lu!@exwyz6tu*5kfqhd%8jwx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
